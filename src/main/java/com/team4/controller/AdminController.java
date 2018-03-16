d007912b34c43e9c9320a6bc86eac9adde1985cc
asuswith <1223272234@qq.com>
2018-03-16 10:52:55 +0800

    Signed-off-by: asuswith <1223272234@qq.com>

--- src/main/java/com/team4/controller/AdminController.java
+++ src/main/java/com/team4/controller/AdminController.java
@@ -2,13 +2,17 @@ package com.team4.controller;
 
 import com.team4.entity.Admin;
 import com.team4.service.AdminService;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
 @Controller
@@ -39,5 +43,42 @@ public class AdminController {
         request.getSession().removeAttribute("id");
         request.getSession().removeAttribute("password");
         return "redirect:/";
+    }
+
+    /**
+     * 显示所有的前台管理员数据
+     * @return  返回用户列表页面
+     */
+    @RequestMapping("/allFrontAdmin")
+    public String getAllAdmin(ModelMap modelMap){
+        List<Admin> admins = adminService.getAllFrontAdmin();
+        modelMap.addAttribute("admins",admins);
+        return "adminList";
+    }
+
+    /**
+     *
+     * @param id
+     * @return  删除结果
+     */
+    @RequestMapping("/delAdmin")
+    @ResponseBody
+    public String delAdmin(String id){
+        return adminService.delAdminById(id).toString();
+    }
+
+    @RequestMapping("/batchDelAdmin")
+    @ResponseBody
+    public String batchDelAdmin( String adminItems) {
+        JSONArray jsonArray = new JSONArray(adminItems);
+        String[] adminItemsId = new String[jsonArray.length()];
+        for(int i = 0; i < jsonArray.length();i++)
+            adminItemsId[i] = (String)jsonArray.get(i);
+        try{
+            adminService.batchDelAdmin(adminItemsId);
+        }catch (RuntimeException e){
+            return "fail";
+        }
+        return "success";
     }
 }
--- src/main/java/com/team4/entity/Admin.java
+++ src/main/java/com/team4/entity/Admin.java
@@ -8,6 +8,7 @@ public class Admin {
     private String email;
     private String id_card;
     private String address;
+    private String date;
     private String password;
     private Integer authority;
 
@@ -65,6 +66,14 @@ public class Admin {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public String getPassword() {
